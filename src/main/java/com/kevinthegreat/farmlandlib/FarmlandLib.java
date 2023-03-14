69cd28e1ecef34ee625e1680d752151e71a394cf
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2023-03-13 21:21:04 -0700

    Setup MixinExtras

--- src/main/java/com/kevinthegreat/farmlandlib/FarmlandLib.java
+++ src/main/java/com/kevinthegreat/farmlandlib/FarmlandLib.java
@@ -1,8 +1,21 @@
 package com.kevinthegreat.farmlandlib;
 
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
-public class FarmlandLib implements ModInitializer {
+public class FarmlandLib implements PreLaunchEntrypoint, ModInitializer {
+    public static final TagKey<Block> FARMLAND = TagKey.of(RegistryKeys.BLOCK, new Identifier("c", "farmlands"));
+
+    @Override
+    public void onPreLaunch() {
+        MixinExtrasBootstrap.init();
+    }
+
     @Override
     public void onInitialize() {
 
